81374d1f77fba7464bb0c8eb16a940e02dd9e49a
dharanicb <dharanicb86391@gmail.com>
2023-05-06 15:19:18 +0000

    solution

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,17 +1,75 @@
-import './index.css'
-// import React from 'react'
-import ReactPlayer from 'react-player'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 
-const VideoPlayer = () => (
-  <div className="video-container">
-    <h1 className="heading">Video Player</h1>
-    <div className="responsive-container">
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-        controls="true"
-      />
-    </div>
-  </div>
-)
+const data = [
+  {
+    group_name: 'Group A',
+    boys: 200,
+    girls: 400,
+  },
+  {
+    group_name: 'Group B',
+    boys: 3000,
+    girls: 500,
+  },
+  {
+    group_name: 'Group C',
+    boys: 1000,
+    girls: 1500,
+  },
+  {
+    group_name: 'Group D',
+    boys: 700,
+    girls: 1200,
+  },
+]
+
+const VideoPlayer = () => {
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="group_name"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar dataKey="boys" name="Boys" fill="#1f77b4" barSize="20%" />
+        <Bar dataKey="girls" name="Girls" fill="#fd7f0e" barSize="20%" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
 
 export default VideoPlayer
